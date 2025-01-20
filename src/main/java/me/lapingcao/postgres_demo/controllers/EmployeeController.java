f184e015af5956e00d90702a74c0691b73c7cbec
Gabe Lapingcao <mgultra2018@gmail.com>
2025-01-20 12:32:04 -0800

    Added EmployeeModelAssembler and updated EmployeeController

--- src/main/java/me/lapingcao/postgres_demo/controllers/EmployeeController.java
+++ src/main/java/me/lapingcao/postgres_demo/controllers/EmployeeController.java
@@ -1,8 +1,34 @@
 package me.lapingcao.postgres_demo.controllers;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.lapingcao.postgres_demo.entities.Employee;
+import me.lapingcao.postgres_demo.entities.EmployeeModelAssembler;
+import me.lapingcao.postgres_demo.repositories.EmployeeRepository;
 
 @RestController
 public class EmployeeController {
-    
+    private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/employees")
+    public CollectionModel<EntityModel<Employee>> all() {
+        // TODO implement
+        return null;
+    }
+
+    @GetMapping("/employees/{id}")
+    public EntityModel<Employee> one(@PathVariable Long id) {
+        // TODO implement
+        return null;
+    }
 }
---
+++ src/main/java/me/lapingcao/postgres_demo/entities/EmployeeModelAssembler.java
@@ -0,0 +1,25 @@
+package me.lapingcao.postgres_demo.entities;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import me.lapingcao.postgres_demo.controllers.EmployeeController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public @NonNull EntityModel<Employee> toModel(@NonNull Employee employee) {
+        return EntityModel.of(
+            employee,
+            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+        );
+    }
+
+}
